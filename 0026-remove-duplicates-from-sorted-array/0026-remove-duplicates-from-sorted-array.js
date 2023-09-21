58ea512be9c48c4c63e2a8e0bd57220a71419bf5
Chia-en Ann Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2023-09-20 20:33:02 -0700

    Time: 78 ms (29.24%), Space: 43.4 MB (99.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -11,8 +11,62 @@ var removeDuplicates = function(nums) {
 
 
     //E.g. given nums = [0,0,1,1,1,2,2,3,3,4], we want [0, 1, 2, 3, 4, _, _, _, _, _]
+
+    //ans * = 1, i . = 1
+    //   .
+    //   *
+    //[0,0,1,1,1,2,2,3,3,4], nums[i] = 0 & nums[i-1] = 0, increment i (.)
+
+    //ans * = 1, i . = 2
+    //     .
+    //   *
+    //[0,0,1,1,1,2,2,3,3,4], nums[i] = 1 & nums[i-1] = 0, replace * with . and increment * & .
+
+    //ans * = 2, i . = 3
+    //       .
+    //     *
+    //[0,1,1,1,1,2,2,3,3,4], nums[i] = 1 & nums[i-1] = 1, increment i (.)
+
+    //ans * = 2, i . = 4
+    //         .
+    //     *
+    //[0,1,1,1,1,2,2,3,3,4], nums[i] = 1 & nums[i-1] = 1, increment i (.)
+
+    //ans * = 2, i . = 5
+    //           .
+    //     *
+    //[0,1,1,1,1,2,2,3,3,4], nums[i] = 2 & nums[i-1] = 1, replace * with . and increment * & .
+
+    //ans * = 3, i . = 6
+    //             .
+    //       *
+    //[0,1,2,1,1,2,2,3,3,4], nums[i] = 2 & nums[i-1] = 2, increment i (.)
+
+
+    //ans * = 3, i . = 7
+    //               .
+    //       *
+    //[0,1,2,1,1,2,2,3,3,4], nums[i] = 3 & nums[i-1] = 2, replace * with . and increment * & .
+
+    //ans * = 4, i . = 8
+    //                 .
+    //         *
+    //[0,1,2,3,1,2,2,3,3,4], nums[i] = 3 & nums[i-1] = 3, increment i (.)
+
+    //ans * = 4, i . = 9
+    //                   .
+    //         *
+    //[0,1,2,3,1,2,2,3,3,4], nums[i] = 4 & nums[i-1] = 3, replace * with . and increment * & .
+
+    //ans * = 5, i . = 9
+    //                     .
+    //           *
+    //[0,1,2,3,4,2,2,3,3,4], i reach the end of array. Return index at *, which is ans = 5 (5 unique elements)
+
+    //let ans = 1. and i = 1, we can compare element at index i to the element right behind it (i-1). If the element at index i and the element right behind it (i-1) are equal, just increment i. 
     
-    //let ans = 1. and i = 1, we can compare element at index i to its prior element. If the element at index i and the prior element do not equal, then we replace the element at index ans with the element at index i and increment ans.
+    //If they are not equal, then we replace the element at index ans with the element at index i and increment ans and i.
+
     let ans = 1;
     let i = 1;
     for(i; i < nums.length; i++) {
