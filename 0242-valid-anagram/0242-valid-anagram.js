a142d0a2fa9798677facca9ef020a1f779ba0e64
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-01-10 21:59:46 -0800

    Time: 83 ms (35.03%), Space: 44 MB (60.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -6,30 +6,30 @@
 var isAnagram = function(s, t) {
 // Solution 1, using a map and counting the characters to see if the two strings match:
     //Time complexity: O(s+t) = O(n). Space complexity: O(s) = O(n)
-//     if(s.length !== t.length) {
-//         return false;
-//     }
+    if(s.length !== t.length) {
+        return false;
+    }
     
-//     const map = new Map();
-//     for(var i = 0; i < s.length; i++) {
-//         if(map.has(s.charAt(i))) {
-//             map.set(s.charAt(i), map.get(s.charAt(i)) + 1);
-//         } else {
-//             map.set(s.charAt(i), 1);
-//         }
-//     }
+    const map = new Map();
+    for(var i = 0; i < s.length; i++) {
+        if(map.has(s.charAt(i))) {
+            map.set(s.charAt(i), map.get(s.charAt(i)) + 1);
+        } else {
+            map.set(s.charAt(i), 1);
+        }
+    }
     
-//     for(var j = 0; j < t.length; j++) {
-//         if(map.has(t.charAt(j)) && map.get(t.charAt(j)) > 0) {
-//             map.set(t.charAt(j), map.get(t.charAt(j)) - 1);
-//         } else if(map.has(t.charAt(j)) && map.get(t.charAt(j)) <= 0) {
-//             return false;
-//         } else {
-//             return false;
-//         }
-//     }
+    for(var j = 0; j < t.length; j++) {
+        if(map.has(t.charAt(j)) && map.get(t.charAt(j)) > 0) {
+            map.set(t.charAt(j), map.get(t.charAt(j)) - 1);
+        } else if(map.has(t.charAt(j)) && map.get(t.charAt(j)) <= 0) {
+            return false;
+        } else {
+            return false;
+        }
+    }
     
-//     return true;
+    return true;
     
 
     //Solution 2:
@@ -40,9 +40,9 @@ var isAnagram = function(s, t) {
     //We can split the string into an array, then sort it, then join the characters back into a string
     //The complexity of split without a string split delimiter is the string itself, so s and t lengths. Therefore O(s) and O(t). Same goes for join with no delimiter.
     
-    if(s.length !== t.length) {
-        return false;
-    }
+//     if(s.length !== t.length) {
+//         return false;
+//     }
     
-    return s.split('').sort().join('') === t.split('').sort().join('');
+//     return s.split('').sort().join('') === t.split('').sort().join('');
 };
