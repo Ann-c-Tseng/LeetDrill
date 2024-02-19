6fbe1f300c173f8260d82deaa29d959ef04c9769
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-02-18 22:27:31 -0800

    Time: 71 ms (51.52%), Space: 52.4 MB (29.23%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    var left = 1;
+    var right = Math.max(...piles);
+    var mid = 0;
+    
+    while(left < right) {
+        var mid = Math.floor((right + left) / 2);
+        //console.log("left: " + left + " " + " + right: " + right);
+        //console.log("mid: " + mid)
+        
+        var hours = 0;
+        for(var i = 0; i < piles.length; i++) {
+            hours += Math.ceil(piles[i]/mid);
+        }
+        //console.log("piles: " + piles[0] + " + hours: " + hours)
+        
+        if(hours <= h) { 
+            //If Koko CAN eat all the bananas under or at the time h
+            //Then we know that at whatever k value mid is
+            //(or under that mid value) is where our solution lies.
+            //Therefore binary search from left to right = mid now.
+            right = mid;
+        } else {
+            //Otherwise search the other half
+            left = mid + 1;
+        }
+    }
+    
+    //If left is greater than or equal to the right, return the left value 
+    //which is the maller k than right k value
+    return left;
+}
