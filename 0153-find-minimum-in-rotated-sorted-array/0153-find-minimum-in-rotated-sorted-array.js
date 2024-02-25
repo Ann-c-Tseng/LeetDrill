117e07d8265ac795954c6680177794fd0218c389
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-02-25 15:58:46 -0800

    Time: 56 ms (39.96%), Space: 48.3 MB (58.68%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    var left = 0;
+    var right = nums.length - 1;
+    var res = nums[0];
+
+    while(left <= right) {
+        var res;
+        if(nums[left] < nums[right]) {
+            //We know we reached the point where we have left
+            //at the minimum element, when left pointer has
+            //value that is smaller than the right value,
+            //returning either res or that value
+            res = Math.min(res, nums[left]);
+            break;
+        }
+        var m = left+Math.floor((right-left)/2)
+        res = Math.min(res, nums[m]);
+        if(nums[m] >= nums[left]) {
+            left = m + 1;
+        } else {
+            right = m - 1;
+        }
+    }
+    return res;
+};
