1711763634c9c6e306bbd3d8aa447493f2deb7bd
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-04-07 20:37:40 -0700

    Time: 60 ms (18.21%), Space: 50.5 MB (5.57%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {    
+    flip(root);
+    return root;
+};
+
+var flip = function(t) {
+    var topPtr = t;
+    if(topPtr && topPtr.right && topPtr.left) {
+        var rPtr = topPtr.right;
+        var lPtr = topPtr.left;
+        
+        console.log(rPtr.val)
+        console.log(lPtr.val)
+    
+        topPtr.left = rPtr;
+        topPtr.right = lPtr;
+        
+        flip(topPtr.left);
+        flip(topPtr.right);
+    } else if(topPtr && topPtr.right) {
+        var rPtr = topPtr.right;
+        
+        topPtr.left = rPtr;
+        topPtr.right = null;
+        
+        flip(topPtr.left);
+    } else if(topPtr && topPtr.left) {
+        var lPtr = topPtr.left;
+        
+        topPtr.right = lPtr;
+        topPtr.left = null;
+        
+        flip(topPtr.right);
+    }
+}
