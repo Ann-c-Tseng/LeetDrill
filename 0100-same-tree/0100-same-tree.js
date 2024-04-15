6b87dd9cb7a125c607724607ec6ea0b40580fff5
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-04-14 21:32:17 -0700

    Time: 56 ms (34.14%), Space: 48.8 MB (86.11%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+/* Time complexity: O(N). Hit each node at most once. */
+var isSameTree = function(p, q) {
+    return check(p, q);
+};
+
+var check = function(p, q) {
+    //Base cases
+    if(!p && !q) { //reach the end of branch and have no differences
+        return true;
+    } else if ((p) && (q) && (p.val !== q.val)) { //values are not equal in !null node
+        return false;
+    } else if((!p && q) || (p && !q)) { //one side is null while the other isn't
+        return false;
+    }
+    
+    //Keep going down the tree to the left and right.
+    return check(p.left, q.left) && check(p.right, q.right);
+}
