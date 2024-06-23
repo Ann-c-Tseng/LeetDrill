38ecf75f78ee8f9d2684f9e51171ecd18e649e65
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-06-23 16:56:37 -0500

    Time: 78 ms (78.33%), Space: 53.9 MB (81.40%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -11,6 +11,9 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
+
+/*
+Initial solution:
 var buildTree = function(preorder, inorder) {
     if (!preorder.length || !inorder.length) return null;
     
@@ -22,3 +25,35 @@ var buildTree = function(preorder, inorder) {
     
     return root;
 };
+The above has O(N^2) time complexity, running through all the values in the subtree O(N) + using slice which at its worst can take up O(N) time complexity. We can reduce this complexity to be O(N) using a hashmap which has lookup that takes constant O(1) time.
+
+Better solution is below, taking O(N) time complexity, with the hashmap lookup being O(1)
+*/
+
+var buildTree = function(preorder, inorder) {
+    var preorderIndex = 0;
+    var inorderIndexMap = new Map();
+    for(var i = 0; i < inorder.length; i++) {
+        inorderIndexMap.set(inorder[i], i);
+    }
+    
+    function arrayToTree(left, right) {
+        //if there are no elements to construct the tree
+        if(left > right) return null;
+        
+        //Get the preorderIndex element as the root and increment it
+        var rootValue = preorder[preorderIndex];
+        var root = new TreeNode(rootValue);
+        preorderIndex++;
+        
+        //Now build the left and right subtrees. 
+        //Remember to exclude the inorderIndexMap[rootValue] 
+        //since that is the root and is already accounted for above.
+        root.left = arrayToTree(left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(inorderIndexMap.get(rootValue) + 1, right);
+        
+        return root;
+    }
+    
+    return arrayToTree(0, preorder.length - 1);
+}
