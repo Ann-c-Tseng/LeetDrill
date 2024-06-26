91600852f4ff91f92ff5e97503e9d98e19c847da
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-06-25 23:18:53 -0500

    Time: 127 ms (5.06%), Space: 58 MB (12.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -22,6 +22,11 @@ var topKFrequent = function(nums, k) {
     var sortedMap = new Map([...frequencyMap].sort((a, b) => b[1] - a[1]));
     
     //Put top k values from the sortedMap into the solution array
+    //Note the spread operator used here, then j grabs the top k
+    //elements, and [0] for the key from the key-value pair.
+    //Spread operator is typically O(N), since it iterates
+    //through the map object and makes a copy for every element,
+    //but this is typically not going to be a bottleneck for most code.
     var solution = [];
     for(var j = 0; j < k; j++) {
         solution[j] = [...sortedMap][j][0];
