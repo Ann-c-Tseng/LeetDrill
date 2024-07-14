9028382421078456bea40acbc85b37d20f0b5216
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-07-14 14:32:34 -0500

    Time: 53 ms (91.39%), Space: 52 MB (73.85%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -5,7 +5,13 @@
 
  /*
  Time complexity: O(N)
- Space complexity: O(N)
+ Space complexity: O(N) (technically O(N+4) because of the OPERATORS map, but simplifies to O(N))
+
+ Use a js object (hashmap) to store key value pairs of operator to arrow functions of the operation given a and b values.
+ Go through the tokens linearly. If we hit a token that is an operator, pop secondValue and firstValue (pop twice) to do the operation.
+ Note how we create a const operation function below to apply the operaton function in the OPERATORS map.
+ The after we got the operation result, push that back to the top of the stack.
+ If we just encounter a number, simply convert the string number to an actual Number type and push it onto the stack.
  */
 
  const OPERATORS = {
