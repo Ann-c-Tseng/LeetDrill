105c36c3501d9c66a12be365c2b373dc1fadf706
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-08-04 15:47:32 -0500

    Time: 95 ms (64.83%), Space: 61.8 MB (70.95%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.js
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.js
@@ -6,6 +6,68 @@
  * }
  */
 
+//Level Order Traversal Queue Solution BELOW:
+/*
+Time-complexity: O(N) - Both serialize and deserialize traverse through the tree node at most once.
+Space-complexity: O(N) - Both serialize and deserialize uses the entire tree structure of N nodes.
+Serialize - Use a queue and traverse level by level the given tree. At each node store into array, grab its left and right child if any.
+Make sure to take care of null nodes with '@' symbol. Use an array to track the node values. Use join(",") to make arr into string.
+Deserialize - Go through the array and also use the queue to attach left and right children to each new nodes.
+*/
+
+var serialize = function(root) {
+    if(!root) return "@";//given an empty tree []
+
+    const queue = [root];
+    const result = [];
+
+    while(queue.length > 0) {
+        const node = queue.shift(); //Pull from the front of the queue to move through tree level-by-level
+        if(node) {
+            result.push(node.val.toString());
+            queue.push(node.left);
+            queue.push(node.right);
+        } else {
+            result.push("@");
+        }
+    }
+    return result.join(",");
+}
+
+var deserialize = function(data) {
+    if(data === "@") return null; //Given tree is empty
+
+    const dataArray = data.split(",");
+
+    const root = new TreeNode(dataArray[0]);
+    const queue = [root];
+    let counter = 1;
+
+    while(queue.length > 0) {
+        const node = queue.shift();
+
+        //Go through left and right child, add to current node.
+        if(dataArray[counter] !== "@") {
+            node.left = new TreeNode(dataArray[counter]);
+            queue.push(node.left);
+        }
+        counter++;
+        if(dataArray[counter] !== "@") {
+            node.right = new TreeNode(dataArray[counter]);
+            queue.push(node.right);
+        }
+        counter++;
+    }
+
+    return root;
+}
+
+
+
+
+
+//IN ORDER DFS Solution BELOW: 
+
  /*
     Time-complexity: O(N) - Both functions traverse through N nodes at most once.
     Space-complexity: O(N) - Both functions we keep the entire tree of N nodes.
@@ -16,13 +78,14 @@
     up the tree. While going through the array, shift() can grab from the front of array.
     RECURSION TAKES CARE OF the node ordering when placing into tree.
  */
-
+/*
 /**
  * Encodes a tree to a single string.
  *
  * @param {TreeNode} root
  * @return {string}
  */
+ /*
 var serialize = function(root) {
     let serializedArray = [];
 
@@ -49,7 +112,7 @@ var serialize = function(root) {
         return "@";
     };
 };
-
+*/
 
 
 /**
@@ -58,6 +121,7 @@ var serialize = function(root) {
  * @param {string} data
  * @return {TreeNode}
  */
+ /*
 var deserialize = function(data) {
     //1,2,@,@,3,4,@,@,5,@,@
     let dataArr = data.split(",");
@@ -83,8 +147,12 @@ var deserialize = function(data) {
 
     return preOrderTraversal();
 };
+*/
+
 
 /**
  * Your functions will be called as such:
  * deserialize(serialize(root));
  */
+
+ 
