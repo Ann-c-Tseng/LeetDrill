e93b4d8e34f2f2e62999f570ec05cc3e7fa0e89d
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-08-13 22:58:48 -0500

    Time: 65 ms (72.73%), Space: 52.8 MB (45.26%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -3,11 +3,13 @@
  * @param {number} target
  * @return {number[][]}
  */
+ /* 
+ Time complexity: O(2^N * N) - sorting is O(N log N) + in worse case each element can be included or excluded which makes O(2^N) + O(N) in worse case where for each valid combination we push a copy of length N to the result array.
+ Space complexity: O(N) - recursive stack takes O(N) space. The current combination's storage can at worse be N length.
+ */
 var combinationSum2 = function(candidates, target) {
     candidates.sort((a,b) => a - b);
-
     res = []
-    
     var backtracking = function(cur, start, target) {
         if(target === 0) {
             res.push([...cur]); //push copy to result if we reached the target
@@ -15,9 +17,7 @@ var combinationSum2 = function(candidates, target) {
         if(target < 0) { //don't wanna go down a path that is greater than the target (therefore target goes into the negatives)
             return;
         }
-
         let prev = -1;
-
         //Consider every candidate in the input
         for(let i = start; i < candidates.length; i++) {
             //For a new path, we will want to skip duplicate numbers, so skip loop iteration
@@ -32,7 +32,6 @@ var combinationSum2 = function(candidates, target) {
             prev = candidates[i];
         }
     }
-
     backtracking([], 0, target);
     return res;
 };
