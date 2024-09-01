202154f6a87d39aeb4a5488ce92bf6aa287b346b
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-09-01 16:27:17 -0500

    Time: 85 ms (34.13%), Space: 55.2 MB (55.67%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.js
+++ 0130-surrounded-regions/0130-surrounded-regions.js
@@ -10,6 +10,7 @@ Depth-First Search: 1. Capture the regions that are connected to the boarder and
 2. Then for all left over regions that are Os, we know they are not boarder regions and can be captured into Xs. 
 3. Convert any T cells which represent boarder regions into Os.
  */
+ /*
 var solve = function(board) {
     const rows = board.length;
     const cols = board[0].length;
@@ -29,7 +30,7 @@ var solve = function(board) {
         dfsCapture(r, c-1);
     }
 
-    //DFS - Capture unsurrounded regions connected to the boarder (O -> T)
+    //1. DFS - Capture unsurrounded regions connected to the boarder (O -> T)
     for(let i = 0; i < rows; i++) {
         for(let j = 0; j < cols; j++) {
             if(board[i][j] === "O" && 
@@ -41,7 +42,7 @@ var solve = function(board) {
         }
     }
 
-    //Capture the surrounded regions (O -> X)
+    //2. Capture the surrounded regions (O -> X)
     for(let k = 0; k < rows; k++) {
         for(let l = 0; l < cols; l++) {
             if(board[k][l] === "O") {
@@ -50,7 +51,7 @@ var solve = function(board) {
         }
     }
 
-    //Uncapture unsurrounded regions (T -> O)
+    //3. Uncapture unsurrounded regions (T -> O)
     for(let k = 0; k < rows; k++) {
         for(let l = 0; l < cols; l++) {
             if(board[k][l] === "T") {
@@ -59,19 +60,65 @@ var solve = function(board) {
         }
     }
 };
+*/
 
  /*
-Time-complexity: O(M * N)
-Space-complexity:
-Breadth-First Search:
+Time-complexity: O(M * N) - For all three phases, we visit each cell at most once.
+Space-complexity: O(M * N) 
+Breadth-First Search: 1. Use queue to explore all O cells connected to the border and make it T.
+2. Then for all left over regions with Os, we know they are not boarder regions and can be captured into Xs. 
+3. Convert any T cells which represent boarder regions into Os.
  */
- /*
 var solve = function(board) {
+    if(board === null || board.length === 0) {
+        return;
+    }
+
     const rows = board.length;
     const cols = board[0].length;
 
+    var bfsCapture = function(r, c) {
+        const q = [];
+        q.push([r, c]);
+        board[r][c] = 'T';
 
+        const directions = [[-1,0], [1,0], [0,-1], [0,1]];
 
+        while(q.length > 0) {
+            const [x,y] = q.shift();
+            
+            for(const [dx, dy] of directions) {
+                const newX = x + dx;
+                const newY = y + dy;
 
+                //If neighbor is within bounds and is an "O", we can push it to the queue and set it to T.
+                if(newX >= 0 && newX < rows && newY >= 0 && newY < cols && board[newX][newY] === "O") {
+                    q.push([newX, newY]);
+                    board[newX][newY] = "T";
+                }
+            }
+        }
+    }
+
+    //1. BFS: Capture all regions connected to the border (O -> T)
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            if(board[i][j] === 'O' && (i === 0 || i === rows - 1 || j === 0 || j === cols - 1)) {
+                bfsCapture(i, j);
+            }
+        }
+    }
+
+    //2. For all nonboarder regions, flip those Os to Xs.
+    //3. Flip all the Ts (border regions) back to Os
+    for(let k = 0; k < rows; k++) {
+        for(let l = 0; l < cols; l++) {
+            if(board[k][l] === "O") {
+                board[k][l] = "X";
+            }
+            if(board[k][l] === "T") {
+                board[k][l] = "O";
+            }
+        }
+    }
 };
-*/
