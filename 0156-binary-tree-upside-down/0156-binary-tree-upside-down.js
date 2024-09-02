48faa0f2bfd1effac3638b0d58b8f5469294d917
Chia-en Tseng <68884155+Ann-c-Tseng@users.noreply.github.com>
2024-09-02 14:46:09 -0500

    Time: 60 ms (34.38%), Space: 51.3 MB (34.38%) - LeetHub

---
+++ 0156-binary-tree-upside-down/0156-binary-tree-upside-down.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var upsideDownBinaryTree = function(root) {
+    let newTree = root;
+    
+    //If a node does not have a left child, set current node to be new tree root.
+    //If we have a left child, make the swap and return the right child (original root)
+    var dfs = function(node) {
+        if(!node.left) {
+            let newRoot = new TreeNode(node.val);
+            newTree = newRoot;
+            return newRoot;
+        }
+
+        if(node.left) {
+            let leftNode = dfs(node.left);
+            leftNode.right = new TreeNode(node.val); //left child's right pointer to parent
+            if(node.right) {
+                leftNode.left = new TreeNode(node.right.val); //left child's left pointer to right child of parent
+            }
+            return leftNode.right; //Continue going up old tree layer by layer
+        }
+    }
+
+    if(root !== null) {
+        dfs(root);
+    }
+    return newTree;
+};
